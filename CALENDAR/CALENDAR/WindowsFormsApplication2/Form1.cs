f9a6eb76651b156138aa91cac44e8c45de04ba5e
U-lev-notebook\lev <lev@lev-notebook.(none)>
2013-07-14 17:15:18 -0400

    made excel calendar prototype actually work

--- CALENDAR/CALENDAR/WindowsFormsApplication2/Form1.cs
+++ CALENDAR/CALENDAR/WindowsFormsApplication2/Form1.cs
@@ -10,25 +10,31 @@ using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Reflection; 
 
+
 namespace WindowsFormsApplication2
 {
     public partial class Form1 : Form
     {
         List<appointment> appointments = new List<appointment>();
 
+        void addAppointment(int tyear, int tmonth, int tday, int tstarttime, int tendtime, string tactivity, string tlocation)
+        {
+            appointments.Add(new appointment(tyear, tmonth, tday, tstarttime, tendtime, tactivity, tlocation));
+        }
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        void addAppointment(int tyear, int tmonth, int tday, int tstarttime, int tendtime, string tactivity, string tlocation)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            appointments.Add(new appointment( tyear, tmonth, tday, tstarttime, tendtime, tactivity, tlocation));
-        }
 
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
+
             Excel.Application oXL;
             Excel._Workbook oWB;
             Excel._Worksheet oSheet;
@@ -65,7 +71,7 @@ namespace WindowsFormsApplication2
             oXL.UserControl = true;
 
         }
-    }
+        }
     public class appointment
     {
         public int year;
@@ -76,67 +82,70 @@ namespace WindowsFormsApplication2
         public string activity;
         public string location;
 
-        public appointment (int tyear, int tmonth, int tday, int tstarttime, int tendtime, string tactivity, string tlocation){
-             year = tyear;
-             month = tmonth;
-             day = tday;
-             startTime = tstarttime;
-             endTime = tendtime;
-             activity = tactivity;
-             location = tlocation;
+        public appointment(int tyear, int tmonth, int tday, int tstarttime, int tendtime, string tactivity, string tlocation)
+        {
+            year = tyear;
+            month = tmonth;
+            day = tday;
+            startTime = tstarttime;
+            endTime = tendtime;
+            activity = tactivity;
+            location = tlocation;
         }
 
-            public string showDate (){
-                string monthName;
-                switch (month)
-                {
-                    case(1):
-                        monthName = "Jan";
-                        break;
-                    case(2):
-                        monthName = "Feb";
-                        break;
-                    case(3):
-                        monthName = "Mar";
-                        break;
-                    case(4):
-                        monthName = "Apr";
-                        break;
-                    case(5):
-                        monthName = "May";
-                        break;
-                    case(6):
-                        monthName = "Jun";
-                        break;
-                    case(7):
-                        monthName = "Jul";
-                        break;
-                    case(8):
-                        monthName = "Aug";
-                        break;
-                    case(9):
-                        monthName = "Sep";
-                        break;
-                    case(10):
-                        monthName = "Oct";
-                        break;
-                    case(11):
-                        monthName = "Nov";
-                        break;
-                    case(12):
-                        monthName = "Dec";
-                        break;
-                    default:
-                        monthName = "" + month;
-                        break;
-                }
-                
-
-                return monthName + " " + day + " " + year;
+        public string showDate()
+        {
+            string monthName;
+            switch (month)
+            {
+                case (1):
+                    monthName = "Jan";
+                    break;
+                case (2):
+                    monthName = "Feb";
+                    break;
+                case (3):
+                    monthName = "Mar";
+                    break;
+                case (4):
+                    monthName = "Apr";
+                    break;
+                case (5):
+                    monthName = "May";
+                    break;
+                case (6):
+                    monthName = "Jun";
+                    break;
+                case (7):
+                    monthName = "Jul";
+                    break;
+                case (8):
+                    monthName = "Aug";
+                    break;
+                case (9):
+                    monthName = "Sep";
+                    break;
+                case (10):
+                    monthName = "Oct";
+                    break;
+                case (11):
+                    monthName = "Nov";
+                    break;
+                case (12):
+                    monthName = "Dec";
+                    break;
+                default:
+                    monthName = "" + month;
+                    break;
             }
+
+
+            return monthName + " " + day + " " + year;
+        }
 
 
 
 
     }
 }
+
